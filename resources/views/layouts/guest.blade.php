bbc79b58bd80d861b44b0f5fd9d62c1a2357d9f6
Daniel Jiménez Cabello <danieljcpya354@gmail.com>
2022-01-17 07:42:30 +0100

    google analytics

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,6 +15,16 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118150984-11"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-118150984-11');
+        </script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
