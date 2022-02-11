80db3784d43e1e3edb03ff3c6a7b8be0fcf0fbd4
Daniel Jiménez <danieljcpya354@gmail.com>
2022-02-11 13:34:21 +0100

    fix isValidURL

--- src/App.js
+++ src/App.js
@@ -72,8 +72,8 @@ export class App extends Component {
   }
 
   isValidURL(string) {
-    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    return (res !== null)
+    var res = /^(ftp|http|https):\/\/[^ "]+$/
+    return res.test(string)
   };
 
 
